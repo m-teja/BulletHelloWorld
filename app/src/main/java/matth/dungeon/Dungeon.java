e4c41299898590474c5d61bfd8b5438d02f66ab5
Matthew Teja <matthew.teja@gmail.com>
2018-12-18 17:04:48 -0800

    first commit

---
+++ app/src/main/java/matth/dungeon/Dungeon.java
@@ -0,0 +1,13 @@
+package matth.dungeon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dungeon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dungeon);
+    }
+}
---
+++ app/src/main/java/matth/dungeon/MainActivity.java
@@ -0,0 +1,20 @@
+package matth.dungeon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeToGame(View view) {
+        Intent intent = new Intent(this, Dungeon.class);
+        startActivity(intent);
+    }
+}
