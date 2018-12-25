ca7759b67f56c1c792c735255d695305f3527aae
Matthew Teja <matthew.teja@gmail.com>
2018-12-24 23:18:59 -0800

    moved image creation to utility

--- app/src/main/java/matth/dungeon/Enemy.java
+++ app/src/main/java/matth/dungeon/Enemy.java
@@ -18,27 +18,8 @@ public class Enemy {
     }
 
     public ImageView spawnSprite(int x, int y, Integer width, Integer height) {
-        ConstraintLayout map = ((Activity) utility.getCon()).findViewById(R.id.enemyLay);
 
-        map.getLayoutParams().height = utility.getScreenHeight();
-        map.getLayoutParams().width = utility.getScreenWidth();
-        map.setX(0);
-        map.setY(0);
-
-        ConstraintSet set = new ConstraintSet();
-
-        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-
-        ImageView sprite = new ImageView(utility.getCon());
-        sprite.setId(View.generateViewId());
-
-
-        sprite.setImageResource(utility.getCon().getResources().getIdentifier(spriteName, "drawable", utility.getCon().getPackageName()));
-        sprite.setLayoutParams(lp);
-        sprite.setX(x);
-        sprite.setY(y);
-
-
+        ImageView sprite = utility.addImage("enemyLay", "square_enemy", x, y);
 
         if (width != null) {
             sprite.setScaleType(ImageView.ScaleType.CENTER_CROP);
@@ -49,23 +30,12 @@ public class Enemy {
             sprite.setScaleType(ImageView.ScaleType.CENTER_CROP);
             sprite.getLayoutParams().height = height;
         }
-
-        map.addView(sprite);
-        set.clone(map);
-        set.connect(sprite.getId(), ConstraintSet.TOP, map.getId(), ConstraintSet.TOP, 0);
-        set.applyTo(map);
-
         return sprite;
 
     }
 
     public ImageView spawnProjectile(int x, int y, Integer width, Integer height) {
         ConstraintLayout map = ((Activity) utility.getCon()).findViewById(R.id.enemyLay);
-
-        map.getLayoutParams().height = utility.getScreenHeight();
-        map.getLayoutParams().width = utility.getScreenWidth();
-        map.setX(0);
-        map.setY(0);
 
         ConstraintSet set = new ConstraintSet();
         ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
--- app/src/main/java/matth/dungeon/Utility.java
+++ app/src/main/java/matth/dungeon/Utility.java
@@ -1,10 +1,15 @@
 package matth.dungeon;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Utility {
 
-    Context con;
+    private Context con;
 
     Utility(Context con) {
         this.con = con;
@@ -20,5 +25,29 @@ public class Utility {
 
     public int getScreenWidth() {
         return con.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    public ImageView addImage(String layoutName, String spriteName, int x, int y) {
+
+        int id = con.getResources().getIdentifier(layoutName, "id", con.getPackageName());
+        ConstraintLayout map = ((Activity)con).findViewById(id);
+        ConstraintSet set = new ConstraintSet();
+        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+
+        ImageView image = new ImageView(con);
+        image.setId(View.generateViewId());
+
+
+        image.setImageResource(con.getResources().getIdentifier(spriteName, "drawable", con.getPackageName()));
+        image.setLayoutParams(lp);
+        image.setX(x);
+        image.setY(y);
+
+        map.addView(image);
+        set.clone(map);
+        set.connect(image.getId(), ConstraintSet.TOP, map.getId(), ConstraintSet.TOP, 0);
+        set.applyTo(map);
+
+        return image;
     }
 }
