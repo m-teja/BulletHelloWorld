86355cb583fa4d1e539a7612adea6bc42139d7a1
Matthew Teja <matthew.teja@gmail.com>
2018-12-30 23:46:16 -0800

    Added player saving

--- app/src/main/java/matth/dungeon/Utility/FileUtility.java
+++ app/src/main/java/matth/dungeon/Utility/FileUtility.java
@@ -14,6 +14,7 @@ import matth.dungeon.GameUI.LevelTile;
 public class FileUtility {
 
     private static final String MAP_FILE = "game.map";
+    private static final String PLAYER_FILE = "game.player";
 
     public static void saveMap(Context con, ArrayList<ArrayList<LevelTile>> map) {
 
@@ -43,5 +44,34 @@ public class FileUtility {
             e.printStackTrace();
         }
         return map;
+    }
+
+    public static void savePlayer(PlayerInfoPassUtility playerInfoPassUtility, Context con) {
+        try {
+            FileOutputStream fos = con.openFileOutput(PLAYER_FILE, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(playerInfoPassUtility);
+            oos.close();
+            fos.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static PlayerInfoPassUtility loadPlayer(Context con) {
+        PlayerInfoPassUtility playerInfoPassUtility = null;
+
+        try {
+            FileInputStream fis = con.openFileInput(PLAYER_FILE);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            playerInfoPassUtility = (PlayerInfoPassUtility) ois.readObject();
+            ois.close();
+            fis.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return playerInfoPassUtility;
     }
 }
