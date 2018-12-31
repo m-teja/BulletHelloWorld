078b783b2008f8be8bf7358afc48297772337bb6
Matthew Teja <matthew.teja@gmail.com>
2018-12-30 16:58:02 -0800

    made saved button change depending on availability of saved game

--- app/src/main/java/matth/dungeon/MainActivity.java
+++ app/src/main/java/matth/dungeon/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import matth.dungeon.GameUI.DungeonActivity;
+import matth.dungeon.Utility.FileUtility;
 import matth.dungeon.Utility.MainUtility;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkSavedAvailable();
+    }
+
+    private void checkSavedAvailable() {
+        if (FileUtility.loadMap(this) != null) {
+            Button button = findViewById(R.id.playSavedButton);
+            button.setVisibility(View.VISIBLE);
+        }
     }
 
     public void changeToGame(View view) {
@@ -25,5 +36,6 @@ public class MainActivity extends AppCompatActivity {
     public void changeToSavedGame(View view) {
         Intent intent = new Intent(this, DungeonActivity.class);
         intent.putExtra(MainUtility.LOAD_SAVED, true);
+        startActivity(intent);
     }
 }
